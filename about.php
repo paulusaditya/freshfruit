4c7ef910e76748b58d4a274db0bfdbd90c5ffbf8
paulusaditya <222410103052@unej.ac.id>
2024-06-08 11:15:53 +0700

    last

--- about.php
+++ about.php
@@ -202,7 +202,7 @@
               </div>
               <div class="item">
                 <div class="testimony-wrap p-4 pb-5">
-                  <div class="user-img mb-5" style="background-image: url(images/person_2.jpg)">
+                  <div class="user-img mb-5" style="background-image: url(images/team/desainer.jpeg)">
                     <span class="quote d-flex align-items-center justify-content-center">
                       <i class="icon-quote-left"></i>
                     </span>
